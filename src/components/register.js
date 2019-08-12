00c8de01b8d9036d4ed9c6742569dfcfed33181b
felishaww <felishamiranda1510@gmail.com>
2019-08-12 14:34:22 +0700

    phone number validation added

--- src/components/register.js
+++ src/components/register.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Formik } from 'formik';
+import MaskedInput from 'react-text-mask';
 
 
 import {
@@ -150,11 +151,11 @@ class RegistrationForm extends React.Component {
                     const errors = {};
                     if (!values.email) {
                         errors.email = "Required";
-                      } else if (
+                    } else if (
                         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-                      ) {
+                    ) {
                         errors.email = "Invalid email address";
-                      }
+                    }
 
                     if (!values.password) {
                         errors.password = 'Fill in login password'
@@ -162,11 +163,17 @@ class RegistrationForm extends React.Component {
                     if (values.password.length <= 6) {
                         errors.password = 'Password harus lebih dari 6 karakter ya'
                     }
-                    if(values.password !== values.confirmPassword){
+                    if (values.password !== values.confirmPassword) {
                         errors.confirmPassword = "Tidak sama"
                     }
-                    if (!values.nickname){
+                    if (!values.nickname) {
                         errors.nickname = 'Fill in nickname'
+                    }
+                    if (!values.residence) {
+                        errors.residence = 'Choose your residence'
+                    }
+                    if(!values.phoneNumber){
+                        errors.phoneNumber = 'Required'
                     }
                     return errors;
                 })}
@@ -189,43 +196,43 @@ class RegistrationForm extends React.Component {
                         <Form className="regist-from" {...formItemLayout} onSubmit={handleSubmit}>
                             <Form.Item label="E-mail">
                                 <Input
-                                     value={values.email}
-                                     onChange={handleChange}
-                                     onBlur={handleBlur}
-                                     id="email"
-                                     type="email"
-                                     placeholder="email"
-                                    />
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    id="email"
+                                    type="email"
+                                    placeholder="email"
+                                />
 
-                                     {errors.email && touched.email && (
+                                {errors.email && touched.email && (
                                     <div className="invalid-feedback">{errors.email}</div>
                                 )}
                             </Form.Item>
                             <Form.Item label="Password" hasFeedback>
-                                <Input.Password 
-                                 value={values.password}
-                                 onChange={handleChange}
-                                 onBlur={handleBlur}
-                                 id="password"
-                                 prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                 type="password"
-                                 placeholder="Password" />
-                                 {errors.password && touched.password && (
+                                <Input.Password
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    id="password"
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="Password" />
+                                {errors.password && touched.password && (
                                     <div className="invalid-feedback">{errors.password}</div>
                                 )}
                             </Form.Item>
                             <Form.Item label="Confirm Password" hasFeedback>
-                                <Input.Password 
-                                onBlur={this.handleConfirmBlur}
-                                value={values.confirmPassword}
-                                 onChange={handleChange}
-                                 onBlur={handleBlur}
-                                 id="confirmPassword"
-                                 type="confirmPassword"/>
+                                <Input.Password
+                                    onBlur={this.handleConfirmBlur}
+                                    value={values.confirmPassword}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    id="confirmPassword"
+                                    type="confirmPassword" />
 
-                                 {errors.confirmPassword && touched.confirmPassword && (
+                                {errors.confirmPassword && touched.confirmPassword && (
                                     <div className="invalid-feedback">{errors.confirmPassword}</div>
-                                )} 
+                                )}
                             </Form.Item>
                             <Form.Item
                                 label={
@@ -237,24 +244,47 @@ class RegistrationForm extends React.Component {
                                     </span>
                                 }
                             >
-                                <Input 
-                                onBlur={this.handleConfirmBlur}
-                                value={values.nickname}
-                                 onChange={handleChange}
-                                 onBlur={handleBlur}
-                                 id="nickname"
-                                 type="nickname"/>
+                                <Input
+                                    onBlur={this.handleConfirmBlur}
+                                    value={values.nickname}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    id="nickname"
+                                    type="nickname" />
 
-{errors.nickname && touched.nickname && (
+                                {errors.nickname && touched.nickname && (
                                     <div className="invalid-feedback">{errors.nickname}</div>
                                 )}
 
                             </Form.Item>
                             <Form.Item label="Habitual Residence">
-                                <Cascader options={residences} />
+                                <Cascader options={residences}
+                                    onBlur={this.handleConfirmBlur}
+                                    value={values.residence}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    id="residence"
+                                    type="residence" />
+
+                                {errors.residence && touched.residence && (
+                                    <div className="invalid-feedback">{errors.residence}</div>
+                                )}
                             </Form.Item>
                             <Form.Item label="Phone Number">
-                                <Input style={{ width: '100%' }} />
+                                <MaskedInput
+                                    mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+                                    guide={false}
+                                    class="input"
+                                    id='phoneNumber'
+                                    placeHolder="Enter a phone number"
+                                    value={values.phoneNumber}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    style={{ width: "100%" }}
+                                />
+                                {errors.phoneNumber && touched.phoneNumber && (
+                                    <div className="invalid-feedback">{errors.phoneNumber}</div>
+                                )}
                             </Form.Item>
                             <Form.Item label="Website">
 
